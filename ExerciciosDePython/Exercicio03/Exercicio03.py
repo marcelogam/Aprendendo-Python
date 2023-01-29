89c1b967057ed3c6dd73e39a3e5a63e61db025c8
marcelogam <106875860+marcelogam@users.noreply.github.com>
2023-01-29 11:34:02 -0300

    commit

---
+++ ExerciciosDePython/Exercicio03/Exercicio03.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+
+n3 = n1 + n2
+print('A soma dos numero e: {}'.format(n3))
---
+++ ExerciciosDePython/Exercicio04/Exercicio04.py
@@ -0,0 +1,4 @@
+variavel = input('Digite qualquer tecla ')
+
+print('E um numero: {}'.format(variavel.isnumeric()))
+print('E uma letra: {}'.format(variavel.isalpha()))
