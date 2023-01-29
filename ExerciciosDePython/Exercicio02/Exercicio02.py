2efbb201ad1f85ecb5ae9a4bd4c4376cad1474f6
marcelogam <106875860+marcelogam@users.noreply.github.com>
2023-01-29 11:11:23 -0300

    Initial Commit

---
+++ ExerciciosDePython/Exercicio02/Exercicio02.py
@@ -0,0 +1,3 @@
+nome = input("Qual o seu nome ? \n")
+sobrenome = input("Qual o seu segundo nome ? \n")
+print("Seja bem vindo, {} ".format(nome) + sobrenome) 
---
+++ PrimeirosComandos/PrimeiroPrograma.py
@@ -0,0 +1,5 @@
+nome = input('Qual o seu nome? \n')
+print("Ola " + nome + " Seja bem vindo")
+idade = input('Qual a sua idadde? \n')
+print("Sua idade e:" + idade)
+
